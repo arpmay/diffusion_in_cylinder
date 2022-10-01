b0480382e5b15c46f973324b2c709d05a24f0f99
Arpit Sen Shrivastava <arpmay@gmail.com>
2022-10-01 18:25:40 +0530

    Added Calculator Tab

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
-
 from dehyroxy_func import C
 
 
-def calc_gui(Notebook, width, height):
+class App(tk.Tk):
+
+    def __init__(self):
 
         def solve(r, t, a, conc, D, label):
             r = float(r)
@@ -15,60 +15,71 @@ def calc_gui(Notebook, width, height):
 
             label.config(text=str(C(r, t, a, conc, D)))
 
-        calc_frame = ttk.Frame(Notebook, width=width, height=height)
-        calc_frame.pack()
-        calc_frame.columnconfigure(0, weight=1)
-        calc_frame.columnconfigure(1, weight=4)
+        super().__init__()
+
+        self.title('Diffusion in a cylinder')
+
+        self.geometry('400x400')
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
 
         # Diffusion coefficient entry
 
-        dif_label = tk.Label(calc_frame, text="Diffusion Coefficient")
+        dif_label = tk.Label(self, text="Diffusion Coefficient")
         dif_label.grid(column=0, row=0)
 
         D = tk.StringVar()
-        dif_entry = tk.Entry(calc_frame, textvariable=D)
+        dif_entry = tk.Entry(self, textvariable=D)
         dif_entry.grid(column=1, row=0, sticky='EW', pady=10, padx=10)
 
         # Radius entry
 
-        dif_label = tk.Label(calc_frame, text="Radius")
+        dif_label = tk.Label(self, text="Radius")
         dif_label.grid(column=0, row=1)
 
         a = tk.StringVar()
-        radius_entry = tk.Entry(calc_frame, textvariable=a)
+        radius_entry = tk.Entry(self, textvariable=a)
         radius_entry.grid(column=1, row=1, sticky='EW', pady=10, padx=10)
 
         # initial concentration entry
 
-        init_conc_label = tk.Label(calc_frame, text="Initial Concentration")
+        init_conc_label = tk.Label(self, text="Initial Concentration")
         init_conc_label.grid(column=0, row=2)
 
         conc = tk.StringVar()
-        conc_entry = tk.Entry(calc_frame, textvariable=conc)
+        conc_entry = tk.Entry(self, textvariable=conc)
         conc_entry.grid(column=1, row=2, sticky='EW', pady=10, padx=10)
 
         # Time entry
 
-        time_label = tk.Label(calc_frame, text="Time")
+        time_label = tk.Label(self, text="Time")
         time_label.grid(column=0, row=3)
 
         t = tk.StringVar()
-        time_entry = tk.Entry(calc_frame, textvariable=t)
+        time_entry = tk.Entry(self, textvariable=t)
         time_entry.grid(column=1, row=3, sticky='EW', pady=10, padx=10)
 
         # radial distance entry
 
-        r_label = tk.Label(calc_frame, text="Radial Dist.")
+        r_label = tk.Label(self, text="Radial Dist.")
         r_label.grid(column=0, row=4)
 
         r = tk.StringVar()
-        r_entry = tk.Entry(calc_frame, textvariable=r)
+        r_entry = tk.Entry(self, textvariable=r)
         r_entry.grid(column=1, row=4, sticky='EW', pady=10, padx=10)
 
         # result label
-        result_label = tk.Label(calc_frame, text="")
+        result_label = tk.Label(self, text="")
         result_label.grid(column=0, row=6, columnspan=2)
 
         # calculate button
-        calc = tk.Button(calc_frame, text="Calculate", command=lambda: solve(r.get(), t.get(), a.get(), conc.get(), D.get(), result_label))
+        calc = tk.Button(self, text="Calculate", command=lambda: solve(r.get(), t.get(), a.get(), conc.get(), D.get(), result_label))
         calc.grid(column=0, columnspan=2, row=5)
+
+
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
---
+++ main.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+from tkinter import ttk
+from dehyroxy_func import C
+# root window
+
+
+root = tk.Tk()
+root.geometry('400x300')
+root.title('Diffusion Sim')
+
+# create a notebook
+notebook = ttk.Notebook(root)
+notebook.pack(pady=10, expand=True)
+
+# create frames
+calculator_frame = ttk.Frame(notebook, width=400, height=280)
+radial_plot_frame = ttk.Frame(notebook, width=400, height=280)
+temporal_plot_frame = ttk.Frame(notebook, width=400, height=200)
+simulation_frame = ttk.Frame(notebook, width=400, height=200)
+
+calculator_frame.pack(fill='both', expand=True)
+radial_plot_frame.pack(fill='both', expand=True)
+temporal_plot_frame.pack(fill='both', expand=True)
+simulation_frame.pack(fill='both', expand=True)
+
+# Calc frame
+
+
+def solve(r, t, a, conc, D, label):
+    r = float(r)
+    t = float(t)
+    a = float(a)
+    conc = float(conc)
+    D = float(D)
+
+    label.config(text=str(C(r, t, a, conc, D)))
+
+
+calc_frame = ttk.Frame(notebook, width=400, height=200)
+calc_frame.pack(fill='both', expand=True)
+calc_frame.columnconfigure(0, weight=1)
+calc_frame.columnconfigure(1, weight=4)
+
+# Diffusion coefficient entry
+
+dif_label = tk.Label(calc_frame, text="Diffusion Coefficient")
+dif_label.grid(column=0, row=0)
+
+D = tk.StringVar()
+dif_entry = tk.Entry(calc_frame, textvariable=D)
+dif_entry.grid(column=1, row=0, sticky='EW', pady=10, padx=10)
+
+# Radius entry
+
+dif_label = tk.Label(calc_frame, text="Radius")
+dif_label.grid(column=0, row=1)
+
+a = tk.StringVar()
+radius_entry = tk.Entry(calc_frame, textvariable=a)
+radius_entry.grid(column=1, row=1, sticky='EW', pady=10, padx=10)
+
+# initial concentration entry
+
+init_conc_label = tk.Label(calc_frame, text="Initial Concentration")
+init_conc_label.grid(column=0, row=2)
+
+conc = tk.StringVar()
+conc_entry = tk.Entry(calc_frame, textvariable=conc)
+conc_entry.grid(column=1, row=2, sticky='EW', pady=10, padx=10)
+
+# Time entry
+
+time_label = tk.Label(calc_frame, text="Time")
+time_label.grid(column=0, row=3)
+
+t = tk.StringVar()
+time_entry = tk.Entry(calc_frame, textvariable=t)
+time_entry.grid(column=1, row=3, sticky='EW', pady=10, padx=10)
+
+# radial distance entry
+
+r_label = tk.Label(calc_frame, text="Radial Dist.")
+r_label.grid(column=0, row=4)
+
+r = tk.StringVar()
+r_entry = tk.Entry(calc_frame, textvariable=r)
+r_entry.grid(column=1, row=4, sticky='EW', pady=10, padx=10)
+
+# result label
+result_label = tk.Label(calc_frame, text="")
+result_label.grid(column=0, row=6, columnspan=2)
+
+# calculate button
+calc = tk.Button(calc_frame, text="Calculate",
+                 command=lambda: solve(r.get(), t.get(), a.get(), conc.get(), D.get(), result_label))
+calc.grid(column=0, columnspan=2, row=5)
+
+# add frames to notebook
+
+notebook.add(calculator_frame, text='Calculator')
+notebook.add(radial_plot_frame, text='Radial Plot')
+notebook.add(temporal_plot_frame, text='Temporal Plot')
+notebook.add(simulation_frame, text='Simulation')
+notebook.add(calc_frame, text="Calculator")
+
+
+
+root.mainloop()
