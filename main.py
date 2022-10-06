43023d7faa04403d6c4e68788c20fb8a2dbaf5b1
Arpit Sen Shrivastava <arpmay@gmail.com>
2022-10-06 11:21:27 +0530

    Simulation tab completed

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from matplotlib.figure import Figure
 import numpy as np
 
 from dehyroxy_func import C
-from plot_data import radial_profile, temporal_profile
+from plot_data import radial_profile, temporal_profile, simulation_profile
 
 matplotlib.use("TkAgg")
 
@@ -122,24 +122,28 @@ x_r = None
 y_r = None
 
 
-def plot_radial(figure, figure_canvas_r, t, a, conc, D, n_point_r):
-    t = float(t)
+def plot_radial(figure, figure_canvas_r, t_str, a, conc, D, n_point_r):
+    print(type(t_str), t_str)
+    t = t_str.split(',')
+
     a = float(a)/1000
     conc = float(conc)
     D = float(D)*(1e-4)
     figure.clf()
     axes = figure.add_subplot()
-    axes.set_xlim([0, 1.1*a*1000])
-    axes.set_ylim([0, 1.1*conc])
-    axes.title.set_text(f'Concentration along radius at t = {int(t)} seconds')
+    axes.set_xlim([0, 1.1 * a * 1000])
+    axes.set_ylim([0, 1.1 * conc])
+    axes.title.set_text(f'Concentration along radius at t = {str(t)} seconds')
     axes.set(xlabel='Radial Distance(mm)', ylabel='Concentration')
-    global x_r
-    global y_r
+    print(type(t))
+    for time in t:
+        time = float(time)
+        global x_r
+        global y_r
+        x_r = np.linspace(0, a*1000, num=int(n_point_r))
+        y_r = radial_profile(time, a, D, conc, int(n_point_r))
+        axes.plot(x_r, y_r)
 
-    x_r = np.linspace(0, a*1000, num=int(n_point_r))
-    y_r = radial_profile(t, a, D, conc, int(n_point_r))
-
-    axes.plot(x_r, y_r)
     figure_canvas_r.draw()
 
 
@@ -410,6 +414,162 @@ calc_t.grid(column=1, columnspan=1, sticky='EW', padx=pdx, pady=pdy, row=4)
 save_t = tk.Button(temporal_plot_frame, text="Save Data", command=lambda: save_data_t())
 save_t.grid(column=2, columnspan=1, sticky='EW', padx=pdx, pady=pdy, row=4)
 
+######################
+# SIMULATION SECTION #
+######################
+
+x_s = None
+y_s = None
+
+
+def plot_simulation(figure, figure_canvas, c_sat, D, a, tin, tout, n_point_r, n=100):
+    a = float(a)/1000 # mm to meter conversion
+    D = float(D)*(1e-4) # cm2/s to m2/s conversion
+    tin = int(tin)
+    tout = int(tout)
+    c_sat = float(c_sat)
+    n_point_r = int(n_point_r)
+    figure.clf()
+    axes = figure.add_subplot()
+    axes.set_xlim([0, 1.1*a*1000])
+    axes.set_ylim([0, 1.1*c_sat])
+    axes.title.set_text(f'Concentration w.r.t radius after hydroxylation for {tin} seconds \n and dehydroxylation for {tout} seconds')
+    axes.set(xlabel='Radius(mm)', ylabel='Concentration')
+
+    global x_s
+    global y_s
+
+    x_s = np.linspace(0, 1000*a, num=n_point_r)
+    y_s = simulation_profile(c_sat, D, a, tin, tout, n_point_r)
+
+    axes.plot(x_s, y_s)
+    figure_canvas.draw()
+
+
+def save_data_s():
+    global x_s
+    global y_s
+    files = [('CSV file', '*.csv'),
+             ('All Files', '*.*')]
+    f = filedialog.asksaveasfile(initialfile='data.csv', filetypes=files, defaultextension='.csv', mode='w')
+    if f is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    y = np.array(y_s)
+    xy_data = np.array([x_s, y])
+    xy_data = xy_data.transpose()
+    np.savetxt(f, xy_data, delimiter=',')
+    f.close()  # `()` was missing.
+
+
+simulation_plot_frame = ttk.Frame(notebook, width=root.winfo_width(), height=root.winfo_height())
+simulation_plot_frame.pack(fill='both', expand=True)
+
+simulation_plot_frame.rowconfigure(0, weight=6)
+simulation_plot_frame.rowconfigure(1, weight=1)
+simulation_plot_frame.rowconfigure(2, weight=1)
+simulation_plot_frame.rowconfigure(3, weight=1)
+
+simulation_plot_frame.columnconfigure(0, weight=1)
+simulation_plot_frame.columnconfigure(1, weight=2)
+simulation_plot_frame.columnconfigure(2, weight=1)
+simulation_plot_frame.columnconfigure(3, weight=2)
+
+# frame for plot
+simulation_plot = ttk.Frame(simulation_plot_frame, width=simulation_plot_frame.winfo_width(),
+                            height=simulation_plot_frame.winfo_height())
+
+# Figure
+figure_s = Figure(figsize=(8, 4.5), dpi=100)
+
+# create FigureCanvasTkAgg object
+figure_canvas_s = FigureCanvasTkAgg(figure_s, simulation_plot)
+
+# create the toolbar
+NavigationToolbar2Tk(figure_canvas_s, simulation_plot)
+
+# create axes
+# axes = figure_t.add_subplot()
+
+figure_canvas_s.get_tk_widget().pack(fill='both', expand=True)
+
+# packing simulation_plot frame
+simulation_plot.grid(row=0, column=0, columnspan=6, sticky="NSEW", padx=pdx)
+simulation_plot['borderwidth'] = 1
+simulation_plot['relief'] = 'solid'
+
+# Diffusion coefficient entry
+
+dif_label_s = tk.Label(simulation_plot_frame, text="Diffusion Coefficient(cm2/s)")
+dif_label_s.grid(column=0, row=1, padx=pdx, pady=pdy, sticky='E')
+
+D_s = tk.StringVar()
+D_s.set("1e-7")
+dif_entry_s = tk.Entry(simulation_plot_frame, textvariable=D_s)
+dif_entry_s.grid(column=1, row=1, sticky='W', padx=pdx, pady=pdy)
+
+# saturation concentration entry
+
+sat_conc_label_s = tk.Label(simulation_plot_frame, text="Saturation Concentration(unit)")
+sat_conc_label_s.grid(column=2, row=1, sticky='E', padx=pdx, pady=pdy)
+
+sat_conc_s = tk.StringVar()
+sat_conc_s.set("10")
+sat_conc_entry_s = tk.Entry(simulation_plot_frame, textvariable=sat_conc_s)
+sat_conc_entry_s.grid(column=3, row=1, sticky='W', pady=pdy, padx=pdx)
+
+# Radius entry
+
+radius_label_s = tk.Label(simulation_plot_frame, text="Radius(mm)")
+radius_label_s.grid(column=0, row=2, padx=pdx, pady=pdy, sticky='E')
+
+a_s = tk.StringVar()
+a_s.set("10")
+radius_entry_s = tk.Entry(simulation_plot_frame, textvariable=a_s)
+radius_entry_s.grid(column=1, row=2, sticky='W', padx=pdx, pady=pdy)
+
+
+# Time in entry
+
+tin_label_s = tk.Label(simulation_plot_frame, text="Hydroxylation time(s)")
+tin_label_s.grid(column=2, row=2, padx=pdx, pady=pdy, sticky='E')
+
+tin_s = tk.StringVar()
+tin_s.set("3600")
+tin_entry_s = tk.Entry(simulation_plot_frame, textvariable=tin_s)
+tin_entry_s.grid(column=3, row=2, sticky='W', pady=pdy, padx=pdx)
+
+# Time out entry
+
+tout_label_s = tk.Label(simulation_plot_frame, text="Dehydroxylation time(s)")
+tout_label_s.grid(column=0, row=3, padx=pdx, pady=pdy, sticky='E')
+
+tout_s = tk.StringVar()
+tout_s.set("9")
+tout_entry_s = tk.Entry(simulation_plot_frame, textvariable=tout_s)
+tout_entry_s.grid(column=1, row=3, sticky='W', padx=pdx, pady=pdy)
+
+# No. of points entry
+n_point_label_s = tk.Label(simulation_plot_frame, text="No. of points")
+n_point_label_s.grid(column=2, row=3, padx=pdx, pady=pdy, sticky='E')
+
+n_point_s = tk.StringVar()
+n_point_s.set('100')
+n_point_entry_s = tk.Entry(simulation_plot_frame, textvariable=n_point_s)
+n_point_entry_s.grid(column=3, row=3, sticky='W', pady=pdy, padx=pdx)
+
+
+# calculate button
+calc_s = tk.Button(
+    simulation_plot_frame, text="Plot",
+    command=lambda: plot_simulation(
+        figure_s, figure_canvas_s, sat_conc_s.get(), D_s.get(), a_s.get(), tin_s.get(), tout_s.get(), n_point_s.get()))
+
+calc_s.grid(column=1, columnspan=1, sticky='EW', padx=pdx, pady=pdy, row=4)
+
+# save data button
+save_s = tk.Button(simulation_plot_frame, text="Save Data", command=lambda: save_data_s())
+save_s.grid(column=2, columnspan=1, sticky='EW', padx=pdx, pady=pdy, row=4)
+
 
 #################
 # ABOUT SECTION #
@@ -428,6 +588,7 @@ heading_label.pack()
 notebook.add(calc_frame, text="Calculator")
 notebook.add(radial_plot_frame, text='Radial Plot')
 notebook.add(temporal_plot_frame, text='Temporal Plot')
+notebook.add(simulation_plot_frame, text="Simulation")
 notebook.add(about_frame, text='About')
 
 
--- simulator.py
+++ simulator.py
@@ -1,20 +1,11 @@
 from hydroxy_func import C as c_in
 from dehyroxy_func import C as c_out
-from matplotlib import pyplot as plt
-# Calculating radial profile after hydroxylation using c_in
+# Simulating the entire hydroxylation and dehydroxylation process
 
-c_sat = 1000  # saturation concentration
-n = 100  # Number of terms of the series to be summed
-D = 8.42e-6  # Diffusion coefficient
-a = 16  # Radius of the preform
-tin= 3600  # Total time of hydroxylation
-tout = 3600 # TOtal time of dehydroxylation
-n_point_r = 1000  # No. of divisions to be made in radius/ no. of points at which C will be calculated
 
-r_plot = []
-for i in range(0, n_point_r):
-    r_plot.append(c_out((i / n_point_r) * a, tout, a, c_in(c_sat, (i / n_point_r) * a, tin, a, D), D))
+def simulation_plot(c_sat=1000, D=8.42e-6, a=16, tin=3600, tout=3600, n_point_r=100, n=100):
+    r_plot = []
+    for i in range(0, n_point_r):
+        r_plot.append(c_out((i / n_point_r) * a, tout, a, c_in(c_sat, (i / n_point_r) * a, tin, a, D, n), D))
+    return r_plot
 
-plt.plot(r_plot)
-
-plt.show()
