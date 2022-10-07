0c02a58c023fbde3bc72d4e7fada3eb08f667fb9
Arpit Sen Shrivastava <arpmay@gmail.com>
2022-10-07 16:46:16 +0530

    Added radius intake

--- plot_data.py
+++ plot_data.py
@@ -24,4 +24,5 @@ def cc_simulation_profile(c_sat=1000, D=8.42e-6, a=16, tin=3600, tout=3600, n_po
         r_plot.append(c_out((i / n_point_r) * a, tout, a, c_in(c_sat, (i / n_point_r) * a, tin, a, D, n), D))
     return r_plot
 
+# radial intake function
 
